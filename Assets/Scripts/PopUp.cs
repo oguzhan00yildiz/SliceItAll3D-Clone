47ce31562d0a89207d1e519e7c0baf177fec2942
Mansur Besleney <besleneymansur@gmail.com>
2023-03-19 22:04:39 +0300

    PopUpText added

---
+++ Assets/Scripts/PopUp.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUp : MonoBehaviour
+{
+    void Update()
+    {
+        Destroy(gameObject, 1f);
+    }
+
+}
