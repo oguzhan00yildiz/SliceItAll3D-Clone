38eb428adf33a7498ee37aba245cce72cfa7d36d
Oguzhan Yildiz <oguzhan00yildiz@gmail.com>
2023-03-24 16:24:22 +0300

    KnifeModel updated

--- Assets/Thirdparty/EzySlice/EzySlice/SlicedHull.cs
+++ Assets/Thirdparty/EzySlice/EzySlice/SlicedHull.cs
@@ -25,9 +25,14 @@ namespace EzySlice {
             GameObject newObject = CreateUpperHull();
 
             if (newObject != null) {
-                newObject.transform.localPosition = original.transform.localPosition;
-                newObject.transform.localRotation = original.transform.localRotation;
+                
+                newObject.transform.localPosition = original.transform.position;
+                newObject.transform.localRotation = original.transform.rotation;
                 newObject.transform.localScale = original.transform.localScale;
+
+                //newObject.transform.localPosition = original.transform.localPosition;
+                //newObject.transform.localRotation = original.transform.localRotation;
+                //newObject.transform.localScale = original.transform.localScale;
 
                 Material[] shared = original.GetComponent<MeshRenderer>().sharedMaterials;
                 Mesh mesh = original.GetComponent<MeshFilter>().sharedMesh;
@@ -64,9 +69,15 @@ namespace EzySlice {
             GameObject newObject = CreateLowerHull();
 
             if (newObject != null) {
-                newObject.transform.localPosition = original.transform.localPosition;
-                newObject.transform.localRotation = original.transform.localRotation;
+
+                newObject.transform.localPosition = original.transform.position;
+                newObject.transform.localRotation = original.transform.rotation;
                 newObject.transform.localScale = original.transform.localScale;
+
+
+                //newObject.transform.localPosition = original.transform.localPosition;
+                //newObject.transform.localRotation = original.transform.localRotation;
+                //newObject.transform.localScale = original.transform.localScale;
 
                 Material[] shared = original.GetComponent<MeshRenderer>().sharedMaterials;
                 Mesh mesh = original.GetComponent<MeshFilter>().sharedMesh;
