3a93700c830d2e7001a623025e3fc7069ce339e2
Oguzhan Yildiz <oguzhan00yildiz@gmail.com>
2023-03-25 18:23:34 +0300

    Score multipler added

--- Assets/Scripts/ScoreText.cs
+++ Assets/Scripts/ScoreText.cs
@@ -19,4 +19,44 @@ public class ScoreText : MonoBehaviour
     {
         score.text = "Score : " + SliceTestScript.SliceTestScriptInstant.score.ToString();
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        int number;
+        if (other.CompareTag("2x"))
+        {
+            number = 2;
+            ScoreMultiplier(number);
+        }
+        else if (other.CompareTag("3x"))
+        {
+            number = 3;
+            ScoreMultiplier(number);
+        }
+        else if (other.CompareTag("4x"))
+        {
+            number =4;
+            ScoreMultiplier(number);
+        }
+        else if (other.CompareTag("5x"))
+        {
+            number =5;
+            ScoreMultiplier(number);
+        }
+        else if (other.CompareTag("10x"))
+        {
+            number = 10;
+            ScoreMultiplier(number);
+        }
+        
+    }
+
+
+    private void ScoreMultiplier(int multipleValue) 
+    {
+        int multipliedScore;
+
+        multipliedScore = SliceTestScript.SliceTestScriptInstant.score * multipleValue; 
+        
+    }
 }
